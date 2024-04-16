1fbef9a5707a549ed326105883b3c3200ac60507
AmerAlsaour <amer.alsaoor@gmail.com>
2024-04-16 16:33:02 +0300

    Table podcast and song  : done // card done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import './App.css';
+import Ycard from './components/Ycard/Ycard';
+import Table3 from './components/Table3/Table3';
+import data from "./spotify_data.history.json"
 
-
-
-
+function extractPodcastData(podcastName){
+ let resultPodcast= []
+ data.map(song =>{
+  if(song.episode_show_name ==podcastName){
+    resultPodcast.push({
+      episodeName :song.episode_name,
+      episodeStatus:song.reason_end == "trackdone"? "finished": "not finished",
+      played:song.ms_played,
+      date:song.ts
+    });
+  }
+ })
+ console.log(resultPodcast);
+ return resultPodcast;
+}
+function extractSongData(songName){
+ let resultSong= []
+ data.map(song =>{
+  if(song.master_metadata_track_name ==songName){
+    resultSong.push({
+      episodeName :song.master_metadata_album_album_name,
+      episodeStatus:song.reason_end == "trackdone"? "finished": "not finished",
+      played:song.ms_played,
+      date:song.ts
+    });
+  }
+ })
+ console.log(resultSong);
+ return resultSong;
+}
 function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path='/artist' element={<Artist/>}/>
           <Route path='/podcast' element={<Podcast/>}/>
           <Route path='/artist' element={<Artist/>}/>
+          <Route path='/artist' element={<Artist/>}/>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <Ycard />
+      <h1>hi: </h1>
+      
+      <Table3 data={extractPodcastData("The Misfits Podcast")} header={["Episode title","Episode status","Played","Date"]}/>
+      <Table3 data={extractSongData("Antidote")} header={["Album title","Episode Status","Played","Date"]}/>
     </div>
   );
 }
---
+++ src/components/Table3/Table3.js
@@ -0,0 +1,35 @@
+import Table from "react-bootstrap/Table";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Table3.css'
+
+function Table3(props) {
+    return (
+        <Table striped bordered hover size="sm" >
+            <thead >
+                <tr >
+                    <th className="Table3cs">Rank</th>
+                    <th className="Table3cs">{props.header[0]}</th>
+                    <th className="Table3cs">{props.header[1]}</th>
+                    <th className="Table3cs">{props.header[2]}</th>
+                    <th className="Table3cs">{props.header[3]}</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    props.data.map((item,index) => (
+                    <>
+                        <tr>
+                            <td>#{index+1}</td>
+                            <td>{item.episodeName}</td>
+                            <td>{item.episodeStatus}</td>
+                            <td>{item.played}</td>
+                            <td>{item.date}</td>
+                        </tr>
+                    </>
+                ))}
+            </tbody>
+        </Table>
+    );
+}
+
+export default Table3;
