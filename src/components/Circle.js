0bcd9d8a85e791249e8cf15beb7f177e0940b6a1
khaledabdalslam99 <khaledabdalslam99@gmail.com>
2024-04-18 11:26:36 -0400

    finished home page

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -8,7 +8,7 @@ function Circle(props){
       {
         data: [
           { id: 0, value: props.data[0],color: 'cyan', label: `${props.labels[0]} ${Math.trunc(props.data[0]/60000)}m` },
-          { id: 1, value: props.data[0],color: 'yellow', label: `${props.labels[1]} ${Math.trunc(props.data[0]/60000)}m` },
+          { id: 1, value: props.data[1],color: 'yellow', label: `${props.labels[1]} ${Math.trunc(props.data[0]/60000)}m` },
           { id: 2, value: props.data[2],color: "green", label: `${props.labels[2]} ${Math.trunc(props.data[2]/60000)}m` },
           { id: 3, value: props.data[3],color: 'orange', label: `${props.labels[3]} ${Math.trunc(props.data[3]/60000)}m` },
         ],
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,67 +1,67 @@
-import React , { useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Header.css';
 import { Navigate } from 'react-router-dom';
 
 
-  const Header = (props) => {
-    const [options, setOptions] = useState([]);
-    const [selectedOption, setSelectedOption] = useState(null);
+const Header = (props) => {
+  const [options, setOptions] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(null);
 
-    const addAllSongs = () => {
-      if (props.songs) {
+  const addAllSongs = () => {
+    if (props.songs) {
       const newOptions = props.songs.map(song => ({ label: song, value: song }));
       setOptions(newOptions);
-      }
-    };
-    useEffect(() => {
-      addAllSongs();
-    }, []);
-  
+    }
+  };
+  useEffect(() => {
+    addAllSongs();
+  }, []);
 
-      const handleChange = (selectedOption) => {
-        setSelectedOption(selectedOption);
-        
-        
+
+  const handleChange = (selectedOption) => {
+    setSelectedOption(selectedOption);
+
+
+  };
+
+  const colourStyles = {
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      return {
+        ...styles,
+        backgroundColor: isFocused ? "#FC244C" : null,
+        color: isFocused ? "white" : "black"
       };
-
-    const colourStyles = {
-      option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-        return {
-          ...styles,
-          backgroundColor: isFocused ? "#FC244C" : null,
-          color: isFocused ? "white": "black"
-        };
-      }
-    };  
+    }
+  };
   return (
     <header className='container-fluid'>
       <div className='row'>
-      <div className='col-6'>
-        <Select 
-        placeholder="search for song"
-        options={options}
-        onChange={handleChange}
-        className='select'
-        styles={colourStyles}
-        />
-      </div>
-      <div className='col-6'>
-        <div className='row'>
-          <div className='col-6'></div>
-          <div className='col-4 userName'>
-            <p>Rafael Alegra</p>
-          </div>
-          <div className='col-2'>
-          <div className='userPhoto'>
-            <a href='./imgs/Rafael.png' target='_blank'>
-              <img src='./imgs/Rafael.png' />
-            </a>
-          </div>
+        <div className='col-6'>
+          <Select
+            placeholder="search for song"
+            options={options}
+            onChange={handleChange}
+            className='select'
+            styles={colourStyles}
+          />
+        </div>
+        <div className='col-6'>
+          <div className='row'>
+            <div className='col-6'></div>
+            <div className='col-4 userName'>
+              <p>Rafael Alegra</p>
+            </div>
+            <div className='col-2'>
+              <div className='userPhoto'>
+                <a href='./imgs/Rafael.png' target='_blank'>
+                  <img src='./imgs/Rafael.png' />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </header>
   )
